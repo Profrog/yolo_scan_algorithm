da6f343b089e4cb7df6763f99bd5522f2d435b92
profrog <ache159@naver.com>
2021-04-27 17:12:32 +0900

    Update convert.py

--- convert.py
+++ convert.py
@@ -116,8 +116,70 @@ while string0:
  string0 = data2.readline()   
 
  
+ #### utm parsing
  
  
+ import serial
+import time
+import os
+import string
+import argparse
+import cv2
+import sys
+import time
+import math
+import re
+import utm
+#from utm.conversion import from_latlon
+
+# 해당 python 파일을 gnss 장비의 로그를 기록한 .txt 파일과 같이 둡니다.
+# 로그 .txt 파일의 이름을 input.txt로 변형합니다
+# linux 명령어 창에서 "python3 convert.py" 명령어를 통하여 본 프로그램을 실행하면 "output.txt"에 정리되어 나옵니다
+
+
+
+data2 = open('LOG_RAILDRONE_UWB_BasePos.txt', 'r')
+data3 = open('LOG_RAILDRONE_UWB_BasePos(out).txt', 'a')
+#data2.readline()
+string0 = data2.readline()
+
+global output
+
+while string0:
+ try: 
+ 
+  '''
+  x_s = string0.find('posX')
+  y_s = string0.find('posY')
+  z_s = string0.find('posZ')
+  
+  s_length = 40 #string length
+    
+  stringx = string0[x_s:x_s + s_length]
+  stringy = string0[y_s:y_s + s_length]
+  stringz = string0[z_s:z_s + s_length] 
+  ''' 
+    
+  linex = re.findall(r'(?<!\.)[-+]?\b\d+\.\d+(?!\.)\b', string0)
+  #linex = re.findall(r'\d', string0)
+  lat = linex[0]
+  lon = linex[1]
+  
+   
+  #output = str(lat)[2:len(str(linex)) - 2] + " " + str(lon)[2:len(str(liney)) - 2] + "\n"   
+  u = utm.from_latlon(float(lat), float(lon))
+  u1 = str(u)[1:len(str(u))-1].split(',')
+  output = u1[0]  + u1[1] + "\n"
+  data3.write(output)
+  # print(linex) 
+   
+ except:
+  print("error " + string0) 
+  
+  
+ 
+ string0 = data2.readline()   
+
  
  
  
