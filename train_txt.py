a8ff9176528cb16c1fb418f933405b00f3a5cd46
profrog <ache159@naver.com>
2021-07-12 11:20:08 +0900

    Update train_txt.py

--- train_txt.py
+++ train_txt.py
@@ -5,13 +5,26 @@ from pathlib import Path
 
 
 global path
-path = "t_image/"
+path = "detourl/"
+
+global name_o
+name_o = "train.txt"
 
 
 file_list = os.listdir(path)
 file_list_py = [file for file in file_list if file.endswith(".jpg")]
-file_a = open("train_a.txt" ,"w+")
+file_list_py2 = [file for file in file_list if file.endswith(".jpeg")]
+file_list_py3 = [file for file in file_list if file.endswith(".png")]
+
+file_a = open(name_o ,"w+")
 
 for test1 in file_list_py:
  file_a.write(path + test1 + "\n")
+
+for test1 in file_list_py2:
+ file_a.write(path + test1 + "\n")
+
+for test1 in file_list_py3:
+ file_a.write(path + test1 + "\n")
  
+file_a.close()
