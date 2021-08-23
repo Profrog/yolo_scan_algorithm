9940dced2eaba4b7582bb5ec7108ae86e1d3d312
profrog <ache159@naver.com>
2021-08-23 14:35:34 +0900

    Update yolo_webcam_count.py

--- yolo_webcam_count.py
+++ yolo_webcam_count.py
@@ -9,6 +9,7 @@ import sys
 import subprocess
 import signal
 import numpy as np
+from datetime import datetime
 
 
 #test0
@@ -24,21 +25,36 @@ layer_names = net.getLayerNames()
 output_layers = [layer_names[i[0] - 1] for i in net.getUnconnectedOutLayers()]
 colors = np.random.uniform(0, 255, size=(len(classes), 3))
 
+
+#############
 count = 0
 attached = 0
 before = 0
-
-# vc0 = cv2.VideoCapture(0)
-vc = cv2.VideoCapture('/home/kjjgo35/PythonHome/darknet/test3.avi') # 0은 노트북 웹캠 2는 usb로 연결된 웹캠
-
 left_bound = 10
 right_bound = 80
-def checkbound(xs, xe):
-    if xs >= left_bound and xs <= right_bound:
-        return True
-    if xe >= left_bound and xe <= right_bound:
-        return True
-    return False
+##############333
+
+# vc0 = cv2.VideoCapture(0)
+vc = cv2.VideoCapture(2) # 0은 노트북 웹캠 2는 usb로 연결된 웹캠
+
+
+global data
+data =  open('fish_logging.txt', 'a')
+
+
+
+def checkbound(label,xs, xe):
+     
+     if(label != "car"):
+      print("other object")
+      return False
+
+     elif xs >= left_bound and xs <= right_bound:
+      print("Detect " + label)
+      data.write(str(datetime.utcnow().strftime('%Y-%m-%d %H:%M:%S.%f')) + " " + label + " " + str(count) + "\n" )
+      return True
+    
+     else: return False
 
 while True :
     #time.sleep(0.1)
@@ -64,11 +80,13 @@ while True :
             ret, frame = vc.read()
             cv2.imshow("Video Window", frame)   
             cv2.imwrite('image.jpg',frame)     
-
             image = cv2.imread('image.jpg')
 
             # image = cv2.resize(image, None, fx=0.4, fy=0.4)
             height, width, channels = image.shape
+            left_bound = 1
+            right_bound = int(width/6)
+
             
             barcodes = pyzbar.decode(image)
             
@@ -96,8 +114,7 @@ while True :
                boxes.append([x, y, w, h])
                confidences.append(float(confidence))
                class_ids.append(class_id)
-
-               
+              
             indexes = cv2.dnn.NMSBoxes(boxes, confidences, 0.5, 0.4)  
             font = cv2.FONT_HERSHEY_PLAIN
             attached = 0
@@ -109,7 +126,7 @@ while True :
               cv2.rectangle(image, (x, y), (x + w, y + h), color, 2)
               cv2.putText(image, label, (x, y + 30), font, 3, color, 3)
               cv2.imshow("Image", image)
-              if checkbound(x,x+w):
+              if checkbound(label,x,x+w):
                 attached += 1
             
             if(attached > before):
@@ -117,22 +134,17 @@ while True :
               print(count)
             before = attached
                 
-            # change option
-            image = cv2.line(image,(left_bound,0),(left_bound,500),(255,255,255),3)
-            image = cv2.line(image,(right_bound,0),(right_bound,500),(255,255,255),3)
+            # change option   
+            cv2.rectangle(image, (left_bound,1), (right_bound, int(height)),(0,0,255), 3)
             cv2.imshow("Image", image)
 
-            # os.system('./darknet detector test cfg/coco.data cfg/yolov4.cfg yolov4.weights image.jpg')
-            # process = subprocess.Popen(..)   # pass cmd and args to the function
-            # process.send_signal(signal.SIGINT)
-            
-
-                                    
+ 
+                                          
             if cv2.waitKey(1) & 0xFF == ord('q'):
                 break
 
-            
         vc.release()
+        data.close()
         cv2.destroyAllWindows()
 
         if chk == 1:
@@ -142,7 +154,4 @@ while True :
             print("access failed")
 
 
-      
-
-     
       
